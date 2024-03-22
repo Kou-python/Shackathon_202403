a283a3340d7524a1741e09e5ff4eacf11e4c05c6
takedahara <engel3shun@gmail.com>
2024-03-22 18:02:49 +0900

    色付けコード

--- obeya.py
+++ obeya.py
@@ -3,7 +3,7 @@ from utils.contour_utils import find_obeya_contours
 from utils.scoring_utils import calculate_obeya_score
 from utils.ranking_utils import rank_obeya_score
 from utils.display_utils import display_obeya_score
-from utils.split_utils import split_image
+from utils.split_utils import split_image,score_and_color
 from utils.synthesis_utils import merge_images
 
 img_path = "./sample.jpg"
@@ -13,20 +13,28 @@ target_height = 600
 # 画像を読み込み、リサイズする
 resized_img = load_and_resize_image(img_path, target_width, target_height)
 
-#画像の分割
-split_images = split_image(resized_img,rows=8,cols=8)
+# 画像を64個に分割する
+split_images = split_image(resized_img, rows=8, cols=8)
 
-#分割画像のスコア出し
+red_sub_images=[]
+
+# 分割された各部分について処理を行う
 for sub_image in split_images:
     # OBEYA 輪郭を検出する
     sub_contours = find_obeya_contours(sub_image)
 
     # OBEYA スコアを計算する
     sub_score = calculate_obeya_score(sub_contours)
+
+
+
+    # ランクに基づいて赤みの濃い画像に変換する
+    red_sub_image = score_and_color(sub_image, sub_score)
+    red_sub_images.append(red_sub_image)
     print(sub_score)
 
-#分割画像を再構築
-image = merge_images(split_images,rows=8,cols=8)
+# すべての部分画像をマージして元のサイズの画像に再構築する
+image = merge_images(red_sub_images, rows=8, cols=8)
 
 # OBEYA 輪郭を検出する
 obeya_contours = find_obeya_contours(resized_img)
@@ -43,4 +51,3 @@ print("ランク付け:", rank)
 
 # 画像を表示する
 display_obeya_score(image, obeya_contours, score, rank)
-
--- utils/split_utils.py
+++ utils/split_utils.py
@@ -1,4 +1,8 @@
 import cv2
+import numpy as np
+from collections import deque
+from statistics import median
+import math
 
 def split_image(image, rows, cols):
 
@@ -28,4 +32,40 @@ def split_image(image, rows, cols):
 
     return sub_images
     
+    
+
+def score_and_color(image, score):
+    # スコアに応じて赤みを調整
+    if score >= 20:
+        alpha_red = 0.8  # 最も強い赤
+    elif score >= 15:
+        alpha_red = 0.6  # 二番目に強い赤
+    elif score >= 10:
+        alpha_red = 0.4  # 三番目に強い赤
+    elif score >= 5:
+        alpha_red = 0.2  # 四番目に強い赤
+    else:
+        alpha_red = 0  # 五番目に強い赤
+
+    # 赤チャンネルを調整
+    red_channel = image[:, :, 2] * (1 + alpha_red*5)  # 赤チャンネルを調整
+
+    # 元の範囲に制限して変換
+    red_channel = np.clip(red_channel, 0, 255).astype(np.uint8)
+
+    # 調整された赤チャンネルをマージして新しい画像を作成
+    new_image = cv2.merge((image[:, :, 0], image[:, :, 1], red_channel))
+
+    return new_image
+
+
+
+
+
+
+
+
+    
+
+
 
